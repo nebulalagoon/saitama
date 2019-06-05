2d00bf31b97c6ae58ab30ac25767d88a71dc9fd9
Marta Henc <atramhenc@gmail.com>
2019-06-05 17:12:53 +0200

    Add files via upload

---
+++ precache-manifest.41b939e15b6ec8439f7be967e10d4454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512a818c26336dab35b791300a62c87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e69693ef9d9746ce628",
+    "url": "/static/css/main.206fdddb.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "6e69693ef9d9746ce628",
+    "url": "/static/js/main.6c8f03b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
